1e663f6a1ae9104ccb160dabb1e049632bbf6f89
Amreet711 <sunny1935@gmail.com>
2026-06-30 16:52:37 +1000

    Number Swap

---
+++ Number Swap.py
@@ -0,0 +1,16 @@
+#Correct numbers 
+cornum1=34
+cornum2=54
+cornum3=82
+cornum4=60
+cornum5=43
+#Incorrect numbers
+incornum1=43
+incornum2=45
+incornum3=28
+#Incorrect average
+incor_average=(43+45+28+60+42)/5
+print("The average of five numbers is", incor_average)
+#Correct average
+cor_average=(34+54+82+60+43)/5
+print("The correct average of five numbers is", cor_average)
